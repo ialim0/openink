182135d2adca8d387d05cf41af3d5eafea7f0fd8
ialimoud <i.alim0229@gmail.com>
2024-06-03 04:13:08 +0000

    updated menu

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -11,8 +11,6 @@ import {
     FaGithub,
     FaLinkedin,
     FaMapMarkerAlt,
-    FaBars,
-    FaTimes
 } from "react-icons/fa";
 
 interface Quote {
@@ -22,11 +20,13 @@ interface Quote {
 
 const quotes: Quote[] = [
     {
-        quote: "The only limit to our realization of tomorrow is our doubts of today.",
+        quote:
+            "The only limit to our realization of tomorrow is our doubts of today.",
         author: "Franklin D. Roosevelt",
     },
     {
-        quote: "The future belongs to those who believe in the beauty of their dreams.",
+        quote:
+            "The future belongs to those who believe in the beauty of their dreams.",
         author: "Eleanor Roosevelt",
     },
     {
@@ -42,14 +42,14 @@ const quotes: Quote[] = [
         author: "Sam Levenson",
     },
     {
-        quote: "The harder you work for something, the greater you&apos;ll feel when you achieve it.",
+        quote:
+            "The harder you work for something, the greater you&apos;ll feel when you achieve it.",
         author: "Unknown",
     },
 ];
 
 const Menu: React.FC = () => {
     const [currentQuote, setCurrentQuote] = useState<Quote>(quotes[0]);
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
     const pathname = usePathname();
 
     useEffect(() => {
@@ -65,24 +65,33 @@ const Menu: React.FC = () => {
 
     return (
         <div className="flex flex-col h-full shadow-lg bg-gray-50 text-gray-800 font-sans">
-            <nav className="p-6 bg-white shadow-md flex justify-between items-center">
-                <div className="text-xl font-bold">Menu</div>
-                <button
-                    className="text-xl md:hidden"
-                    onClick={() => setIsMenuOpen(!isMenuOpen)}
-                >
-                    {isMenuOpen ? <FaTimes /> : <FaBars />}
-                </button>
+            <nav className="p-6 bg-white shadow-md">
+                <ul className="flex justify-center space-x-8">
+                    {["about", "blog"].map((link) => (
+                        <li key={link}>
+                            <Link href={`/${link}`} passHref>
+                                <span
+                                    className={`text-lg font-semibold transition-colors duration-300 ${isActive(`/${link}`)
+                                        ? "text-blue-800 border-b-2 border-blue-800"
+                                        : "text-blue-600 hover:text-blue-800 focus:outline-none focus:ring-2 focus:ring-blue-600"
+                                        }`}
+                                >
+                                    {link.charAt(0).toUpperCase() + link.slice(1)}
+                                </span>
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
             </nav>
 
-            <div className={`p-6 bg-white rounded-lg shadow-md mt-6 ${isMenuOpen ? 'block' : 'hidden'} md:block`}>
+            <div className="p-6 bg-white rounded-lg shadow-md mt-6">
                 <blockquote className="italic text-gray-600">
                     &ldquo;{currentQuote.quote}&rdquo;
                     <span className="ml-2 font-semibold">- {currentQuote.author}</span>
                 </blockquote>
             </div>
 
-            <div className={`flex-grow overflow-y-auto ${isMenuOpen ? 'block' : 'hidden'} md:block`}>
+            <div className="flex-grow overflow-y-auto">
                 <div className="flex flex-col items-center p-6 bg-white rounded-lg shadow-md mt-6">
                     <div className="w-40 h-40 overflow-hidden rounded mb-4">
                         <Image
@@ -124,57 +133,57 @@ const Menu: React.FC = () => {
                                 Frameworks & Tools
                             </h4>
                             <ul className="list-disc list-inside text-sm text-gray-600 space-y-1">
-                                <li>React</li>
+                                <li>React </li>
                                 <li>Next.js</li>
                                 <li>Git</li>
                             </ul>
                         </div>
                     </div>
                 </div>
-
-                <div className="p-6 bg-white rounded-lg shadow-md mt-6">
-                    <h3 className="text-lg font-semibold mb-4">Language Proficiency</h3>
-                    <div className="bg-gray-100 p-4 rounded-lg">
-                        <h4 className="text-md font-semibold mb-2 flex items-center">
-                            <FaLanguage className="mr-2 text-blue-600" />
-                            Languages
-                        </h4>
-                        <ul className="list-disc list-inside text-sm text-gray-600 space-y-1">
-                            <li>English - Fluent</li>
-                            <li>French - Native</li>
-                            <li>Foodo - Native</li>
-                            <li>Fon - Fluent</li>
-                            <li>Dendi - Fluent</li>
-                        </ul>
+                <div className="flex flex-wrap">
+                    <div className="p-6 bg-white rounded-lg shadow-md mt-6">
+                        <h3 className="text-lg font-semibold mb-4">Language Proficiency</h3>
+                        <div className="bg-gray-100 p-4 rounded-lg">
+                            <h4 className="text-md font-semibold mb-2 flex items-center">
+                                <FaLanguage className="mr-2 text-blue-600" />
+                                Languages
+                            </h4>
+                            <ul className="list-disc list-inside text-sm text-gray-600 space-y-1">
+                                <li>English - Fluent</li>
+                                <li>French - Native</li>
+                                <li>Foodo - Native</li>
+                                <li>Fon - Fluent</li>
+                                <li>Dendi - Fluent</li>
+                            </ul>
+                        </div>
                     </div>
-                </div>
-
-                <div className="p-6 bg-white rounded-lg shadow-md mt-6">
-                    <h3 className="text-lg font-semibold mb-4">Contact Info</h3>
-                    <div className="bg-gray-100 p-4 rounded-lg">
-                        <div className="flex items-center space-x-2">
-                            <FaEnvelope className="text-blue-600" />
-                            <span className="text-sm">
-                                <a href="mailto:i.alim0229@gmail.com">i.alim0229@gmail.com</a>
-                            </span>
-                        </div>
-                        <div className="flex items-center space-x-2 mt-2">
-                            <FaMapMarkerAlt className="text-blue-600" />
-                            <span className="text-sm">Senegal, Dakar</span>
-                        </div>
-                        <div className="flex items-center space-x-2 mt-2">
-                            <FaGithub className="text-blue-600" />
-                            <span className="text-sm">
-                                <a href="https://github.com/ialim0">GitHub</a>
-                            </span>
-                        </div>
-                        <div className="flex items-center space-x-2 mt-2">
-                            <FaLinkedin className="text-blue-600" />
-                            <span className="text-sm">
-                                <a href="https://www.linkedin.com/in/ialim">
-                                    LinkedIn
-                                </a>
-                            </span>
+                    <div className="p-6 bg-white rounded-lg shadow-md mt-6 mr-6">
+                        <h3 className="text-lg font-semibold mb-4">Contact Info</h3>
+                        <div className="bg-gray-100 p-4 rounded-lg">
+                            <div className="flex items-center space-x-2">
+                                <FaEnvelope className="text-blue-600" />
+                                <span className="text-sm">
+                                    <a href="mailto:i.alim0229@gmail.com">i.alim0229@gmail.com</a>
+                                </span>
+                            </div>
+                            <div className="flex items-center space-x-2 mt-2">
+                                <FaMapMarkerAlt className="text-blue-600" />
+                                <span className="text-sm">Senegal, Dakar</span>
+                            </div>
+                            <div className="flex items-center space-x-2 mt-2">
+                                <FaGithub className="text-blue-600" />
+                                <span className="text-sm">
+                                    <a href="https://github.com/ialim0">GitHub</a>
+                                </span>
+                            </div>
+                            <div className="flex items-center space-x-2 mt-2">
+                                <FaLinkedin className="text-blue-600" />
+                                <span className="text-sm">
+                                    <a href="https://www.linkedin.com/in/ialim">
+                                        LinkedIn
+                                    </a>
+                                </span>
+                            </div>
                         </div>
                     </div>
                 </div>
