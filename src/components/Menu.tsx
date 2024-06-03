31372edd0126c66826690313d6d02b93ad3943fa
ialimoud <i.alim0229@gmail.com>
2024-06-03 11:48:50 +0000

    updated menu

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -115,119 +115,114 @@ const Menu: React.FC = () => {
                 </div>
             </nav>
 
-            <div className={`flex-grow overflow-y-auto ${showMenu ? 'block' : 'hidden'} md:block`}>
-                {showMenu && (
-                    <>
-                        <div className="p-6 bg-white rounded-lg shadow-md mt-6">
-                        <blockquote className="italic text-gray-600">
-                                &ldquo;{currentQuote.quote}&rdquo;
-                                <span className="ml-2 font-semibold">- {currentQuote.author}</span>
-                            </blockquote>
-                        </div>
+            <div className={`flex-grow overflow-y-auto md:block ${showMenu ? 'block' : 'hidden'} md:block`}>
+                <div className="p-6 bg-white rounded-lg shadow-md mt-6">
+                    <blockquote className="italic text-gray-600">
+                        &ldquo;{currentQuote.quote}&rdquo;
+                        <span className="ml-2 font-semibold">- {currentQuote.author}</span>
+                    </blockquote>
+                </div>
 
-                        <div className="flex flex-col items-center p-6 bg-white rounded-lg shadow-md mt-6">
-                            <div className="w-40 h-40 overflow-hidden rounded mb-4">
-                                <Image
-                                    src="/images/profile.jpg"
-                                    alt="Profile Image"
-                                    width={200}
-                                    height={200}
-                                    className="object-cover"
-                                />
-                            </div>
-                            <div className="text-center">
-                                <h2 className="text-2xl font-bold">Alimoudine IDRISSOU</h2>
-                                <p className="text-sm">Full Stack Developer</p>
-                                <p className="text-sm mt-2">
-                                    Building innovative solutions for the web and beyond.
-                                </p>
-                            </div>
-                        </div>
+                <div className="flex flex-col items-center p-6 bg-white rounded-lg shadow-md mt-6">
+                    <div className="w-40 h-40 overflow-hidden rounded mb-4">
+                        <Image
+                            src="/images/profile.jpg"
+                            alt="Profile Image"
+                            width={200}
+                            height={200}
+                            className="object-cover"
+                        />
+                    </div>
+                    <div className="text-center">
+                        <h2 className="text-2xl font-bold">Alimoudine IDRISSOU</h2>
+                        <p className="text-sm">Full Stack Developer</p>
+                        <p className="text-sm mt-2">
+                            Building innovative solutions for the web and beyond.
+                        </p>
+                    </div>
+                </div>
 
-                        <div className="p-6 bg-white rounded-lg shadow-md mt-6">
-                            <h3 className="text-lg font-semibold mb-4">Tech Skills</h3>
-                            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-                                <div className="bg-gray-100 p-4 rounded-lg">
-                                    <h4 className="text-md font-semibold mb-2 flex items-center">
-                                        <FaCode className="mr-2 text-blue-600" />
-                                        Programming Languages
-                                    </h4>
-                                    <ul className="list-disc list-inside text-sm text-gray-600 space-y-1">
-                                        <li>Golang</li>
-                                        <li>Rust</li>
-                                        <li>Python</li>
-                                        <li>JavaScript</li>
-                                        <li>Solidity</li>
-                                    </ul>
-                                </div>
-                                <div className="bg-gray-100 p-4 rounded-lg">
-                                    <h4 className="text-md font-semibold mb-2 flex items-center">
-                                        <FaTools className="mr-2 text-blue-600" />
-                                        Frameworks & Tools
-                                    </h4>
-                                    <ul className="list-disc list-inside text-sm text-gray-600 space-y-1">
-                                        <li>React</li>
-                                        <li>Next.js</li>
-                                        <li>Git</li>
-                                    </ul>
-                                </div>
-                            </div>
+                <div className="p-6 bg-white rounded-lg shadow-md mt-6">
+                    <h3 className="text-lg font-semibold mb-4">Tech Skills</h3>
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                        <div className="bg-gray-100 p-4 rounded-lg">
+                            <h4 className="text-md font-semibold mb-2 flex items-center">
+                                <FaCode className="mr-2 text-blue-600" />
+                                Programming Languages
+                            </h4>
+                            <ul className="list-disc list-inside text-sm text-gray-600 space-y-1">
+                                <li>Golang</li>
+                                <li>Rust</li>
+                                <li>Python</li>
+                                <li>JavaScript</li>
+                                <li>Solidity</li>
+                            </ul>
                         </div>
-
-                        <div className="p-6 bg-white rounded-lg shadow-md mt-6 flex flex-wrap">
-                            <div className="p-6 bg-white rounded-lg shadow-md mt-6">
-                                <h3 className="text-lg font-semibold mb-4">Language Proficiency</h3>
-                                <div className="bg-gray-100 p-4 rounded-lg">
-                                    <h4 className="text-md font-semibold mb-2 flex items-center">
-                                        <FaLanguage className="mr-2 text-blue-600" />
-                                        Languages
-                                    </h4>
-                                    <ul className="list-disc list-inside text-sm text-gray-600 space-y-1">
-                                        <li>English - Fluent</li>
-                                        <li>French - Native</li>
-                                        <li>Foodo - Native</li>
-                                        <li>Fon - Fluent</li>
-                                        <li>Dendi - Fluent</li>
-                                    </ul>
-                                </div>
-                            </div>
-
-                            <div className ="p-6 bg-white rounded-lg shadow-md mt-6">
-                                <h3 className="text-lg font-semibold mb-4">Contact Information</h3>
-                                <div className="bg-gray-100 p-4 rounded-lg">
-                                    <ul className="list-none space-y-2">
-                                        <li className="flex items-center">
-                                            <FaEnvelope className="mr-2 text-blue-600" />
-                                            <a href="mailto:alimoudine.idrissou@example.com" className="text-sm text-gray-600">
-                                                ialim
-                                            </a>
-                                        </li>
-                                        <li className="flex items-center">
-                                            <FaGithub className="mr-2 text-blue-600" />
-                                            <a href="https://github.com/ialim" className="text-sm text-gray-600" target="_blank" rel="noopener noreferrer">
-                                                ialim
-                                            </a>
-                                        </li>
-                                        <li className="flex items-center">
-                                            <FaLinkedin className="mr-2 text-blue-600" />
-                                            <a href="https://linkedin.com/in/ialim0" className="text-sm text-gray-600" target="_blank" rel="noopener noreferrer">
-                                                ialim
-                                            </a>
-                                        </li>
-                                        <li className="flex items-center">
-                                            <FaMapMarkerAlt className="mr-2 text-blue-600" />
-                                            <span className="text-sm text-gray-600">Dakar, Senegal</span>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div>
+                        <div className="bg-gray-100 p-4 rounded-lg">
+                            <h4 className="text-md font-semibold mb-2 flex items-center">
+                                <FaTools className="mr-2 text-blue-600" />
+                                Frameworks & Tools
+                            </h4>
+                            <ul className="list-disc list-inside text-sm text-gray-600 space-y-1">
+                                <li>React</li>
+                                <li>Next.js</li>
+                                <li>Git</li>
+                            </ul>
                         </div>
-                    </>
-                )}
+                    </div>
+                </div>
+
+                <div className="p-6 bg-white rounded-lg shadow-md mt-6 flex flex-wrap">
+                    <div className="p-6 bg-white rounded-lg shadow-md mt-6">
+                        <h3 className="text-lg font-semibold mb-4">Language Proficiency</h3>
+                        <div className="bg-gray-100 p-4 rounded-lg">
+                            <h4 className="text-md font-semibold mb-2 flex items-center">
+                                <FaLanguage className="mr-2 text-blue-600" />
+                                Languages
+                            </h4>
+                            <ul className="list-disc list-inside text-sm text-gray-600 space-y-1">
+                                <li>English - Fluent</li>
+                                <li>French - Native</li>
+                                <li>Foodo - Native</li>
+                                <li>Fon - Fluent</li>
+                                <li>Dendi - Fluent</li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div className="p-6 bg-white rounded-lg shadow-md mt-6">
+                        <h3 className="text-lg font-semibold mb-4">Contact Information</h3>
+                        <div className="bg-gray-100 p-4 rounded-lg">
+                            <ul className="list-none space-y-2">
+                                <li className="flex items-center">
+                                    <FaEnvelope className="mr-2 text-blue-600" />
+                                    <a href="mailto:alimoudine.idrissou@example.com" className="text-sm text-gray-600">
+                                        ialim
+                                    </a>
+                                </li>
+                                <li className="flex items-center">
+                                    <FaGithub className="mr-2 text-blue-600" />
+                                    <a href="https://github.com/ialim" className="text-sm text-gray-600" target="_blank" rel="noopener noreferrer">
+                                        ialim
+                                    </a>
+                                </li>
+                                <li className="flex items-center">
+                                    <FaLinkedin className="mr-2 text-blue-600" />
+                                    <a href="https://linkedin.com/in/ialim0" className="text-sm text-gray-600" target="_blank" rel="noopener noreferrer">
+                                        ialim0
+                                    </a>
+                                </li>
+                                <li className="flex items-center">
+                                    <FaMapMarkerAlt className="mr-2 text-blue-600" />
+                                    <span className="text-sm text-gray-600">Senegal, Dakar</span>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
 };
 
 export default Menu;
-
