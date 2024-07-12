8583f1488cf7b9a7642ae27bdf4e3239fe265519
ialimoud <i.alim0229@gmail.com>
2024-07-12 08:49:16 +0000

    data

--- src/components/LayoutWrapper.tsx
+++ src/components/LayoutWrapper.tsx
@@ -29,7 +29,7 @@ const LayoutContent: React.FC<LayoutWrapperProps> = ({ children }) => {
         <div className="container mx-auto px-6 py-3 flex justify-between items-center">
           <div className="flex items-center space-x-2">
             <FaCode className="text-blue-500 text-2xl" />
-            <span className="text-xl font-bold">Welcome </span>
+            <span className="text-xl font-bold">Alimoudine I. </span>
           </div>
           <div className="md:hidden">
             <button
