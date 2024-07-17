38705a6fe35ab63a5e7fc2309318c367177506b4
ialimoud <i.alim0229@gmail.com>
2024-07-17 07:00:13 +0000

    code

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -9,21 +9,16 @@ import { useDarkMode } from "@/context/DarkModeContext";
 const AboutPage: React.FC = () => {
   const { darkMode } = useDarkMode();
 
-  const containerStyles = `max-w-4xl mx-auto p-8 rounded-lg shadow-md ${
-    darkMode ? "bg-gray-800 text-gray-100" : "bg-white text-gray-900"
-  }`;
-  const textStyles = `text-lg mb-4 leading-relaxed ${
-    darkMode ? "text-gray-300" : "text-gray-700"
-  }`;
-  const headingStyles = `text-3xl font-bold mb-6 ${
-    darkMode ? "text-gray-100" : "text-gray-900"
-  }`;
-  const subheadingStyles = `text-xl font-semibold mb-4 ${
-    darkMode ? "text-gray-200" : "text-gray-800"
-  }`;
-  const linkStyles = `inline-flex items-center ${
-    darkMode ? "text-blue-400 hover:text-blue-300" : "text-blue-600 hover:text-blue-800"
-  } font-semibold transition-colors duration-300`;
+  const containerStyles = `max-w-4xl mx-auto p-8 rounded-lg shadow-md ${darkMode ? "bg-gray-800 text-gray-100" : "bg-white text-gray-900"
+    }`;
+  const textStyles = `text-lg mb-4 leading-relaxed ${darkMode ? "text-gray-300" : "text-gray-700"
+    }`;
+  const headingStyles = `text-3xl font-bold mb-6 ${darkMode ? "text-gray-100" : "text-gray-900"
+    }`;
+  const subheadingStyles = `text-xl font-semibold mb-4 ${darkMode ? "text-gray-200" : "text-gray-800"
+    }`;
+  const linkStyles = `inline-flex items-center ${darkMode ? "text-blue-400 hover:text-blue-300" : "text-blue-600 hover:text-blue-800"
+    } font-semibold transition-colors duration-300`;
   const imageContainerStyles = "flex justify-center items-center mb-4";
 
   return (
@@ -37,37 +32,42 @@ const AboutPage: React.FC = () => {
       </Head>
       <div className={`px-6 py-8 ${darkMode ? "bg-gray-900" : "bg-gray-100"} min-h-screen`}>
         <div className={containerStyles}>
-      
+
           <section className="mt-8">
             <h1 className={headingStyles}>Bio</h1>
             <article>
               <p className={textStyles}>
-                My journey began when I discovered a deep fascination with numbers and patterns. As a child, I would spend hours solving complex math problems, captivated by the elegance and logic of the discipline. This passion only grew stronger as I progressed through school, and by the time I was 19, I had already begun sharing my knowledge with others as a high school math teacher.
+                As a child, I found myself drawn to mathematics. This interest led me down an unexpected path - at 19, I had the opportunity to teach high school math. It was challenging, but I learned so much from my students.
               </p>
               <p className={textStyles}>
-                While I found great joy in helping students unlock the mysteries of mathematics, I soon realized that I wanted to apply my analytical skills to tackle challenges on a much broader scale. It was then that I discovered the transformative power of technology, and I knew that I had to be a part of this revolution.
+                Over time, I began to wonder if there were ways to apply mathematical thinking to larger problems. Seeking to deepen my understanding, two B.Sc. degrees - one in Mathematics and Computer Science, another in Planning Sciences - followed by an M.Sc. in Digital Finance. These studies gave me a strong theoretical foundation, but I felt there was still more to learn about practical application in the tech industry.
+
               </p>
               <p className={textStyles}>
-                I dove headfirst into the world of software engineering, driven by an insatiable curiosity and a desire to learn. I mastered a diverse array of programming languages, from the elegant simplicity of Golang to the raw power of Rust and Solidity. I also honed my skills in the versatile Python and the dynamic React framework, allowing me to contribute to a wide range of innovative projects.
+                That's when I discovered the <a href="https://01-edu.org/" target="_blank" rel="noopener noreferrer">01 Edu System</a>, an innovative curriculum in software engineering and programming. Despite my academic background, I embraced this opportunity to enhance my practical skills.
               </p>
               <p className={textStyles}>
-                My academic background, which includes an M.Sc. in Digital Finance and a B.Sc. in Mathematics Fundamentals, has provided me with a solid theoretical foundation to support my technical prowess. But I believe that true excellence comes not just from knowledge, but from the ability to apply it in creative and unexpected ways.
+                The 01 Edu System's approach was unlike anything I'd experienced before. It offered a collective and co-creative learning process in a professional environment. This industry-leading program opened my eyes to the practical skills needed in the digital world and technology industry. It was challenging, but it ignited a new passion for software engineering that continues to drive me today.
               </p>
               <p className={textStyles}>
-                To that end, I have sought out experiences that challenge me both physically and mentally. I have pushed my limits through endurance activities like marathon running, football (soccer), and basketball, learning the value of teamwork, perseverance, and a strong work ethic. I also enjoy friendly competition through video games like PES or FIFA, where I can put my strategic thinking and quick reflexes to the test.
+              Through 01 Edu, I honed my skills in Golang, Python, Rust, and JavaScript. Each language presented unique challenges and rewards, expanding my perspective on technology's potential.
               </p>
               <p className={textStyles}>
-                As I continue on my journey, I am driven by a desire to make a meaningful impact on the world around me. I believe that my combination of technical knowledge, problem-solving mindset, and commitment to teamwork can be a powerful asset to any organization. I am always eager to learn and grow, to tackle new challenges, and to push the boundaries of what is possible.
+              Balancing technical growth with personal development, I embraced endurance sports like marathon running, and team sports such as football and basketball. These pursuits taught me invaluable lessons in perseverance and teamwork. For mental agility, I enjoy strategic video games like PES and FIFA, which sharpen my decision-making and reflexes.
               </p>
               <p className={textStyles}>
-                So if you're curious to learn more about my story, to explore the depths of my technical expertise, or to discover how I might be able to contribute to your team, I invite you to dive deeper. Let's embark on an adventure together, and see what amazing things we can create.
+              As a software engineer, I remain committed to continuous learning. My diverse academic background, coupled with 01 Edu's practical training, has equipped me well for the challenges ahead. I'm eager to contribute to innovative projects and collaborate with others who share my passion for leveraging technology to solve real-world problems.
               </p>
+              <p className={textStyles}>
+              This journey has been full of surprises and challenges, and I'm sure there are many more ahead. But thanks to the strong foundation I've received, I'm excited to face them, one line of code at a time.
+              </p>
+          
             </article>
           </section>
           <section className="mt-12">
             <h2 className={headingStyles}>When I'm not coding</h2>
             <p className={textStyles}>
-              When I'm not coding, I enjoy engaging in activities that help me relax and rejuvenate. Here are some glimpses of my life outside of coding:
+              When I'm not coding, I enjoy engaging in activities that help me relax and rejuvenate. Here are some glimpses of my life outside of coding (2022)
             </p>
             <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 mt-6">
               <div className={imageContainerStyles}>
