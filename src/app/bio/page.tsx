1375a3b8c28f9d3b4c816a73fcc9462a0df0bf2f
ialimoud <i.alim0229@gmail.com>
2024-08-06 00:20:14 +0000

    port_folio

--- src/app/bio/page.tsx
+++ src/app/bio/page.tsx
@@ -1,123 +1,83 @@
 "use client"
-import React from "react";
-import Image from "next/image";
-import Link from "next/link";
-import Head from "next/head";
-import { useDarkMode } from "@/context/DarkModeContext";
+
+import React from "react"
+import Image from "next/image"
+import { useDarkMode } from "@/context/DarkModeContext"
 
 const AboutPage: React.FC = () => {
-  const { darkMode } = useDarkMode();
+  const { darkMode } = useDarkMode()
 
   const styles = {
-    container: `max-w-4xl mx-auto p-8 rounded-lg shadow-md ${darkMode ? "bg-gray-800 text-gray-100" : "bg-white text-gray-900"
-      }`,
-    text: `text-lg mb-4 leading-relaxed ${darkMode ? "text-gray-300" : "text-gray-700"
-      }`,
-    heading: `text-3xl font-bold mb-6 ${darkMode ? "text-gray-100" : "text-gray-900"
-      }`,
-    subheading: `text-2xl font-semibold mb-4 ${darkMode ? "text-gray-200" : "text-gray-800"
-      }`,
-    link: `inline-flex items-center ${darkMode ? "text-blue-400 hover:text-blue-300" : "text-blue-600 hover:text-blue-800"
-      } font-semibold transition-colors duration-300`,
-    imageContainer: "flex justify-center items-center mb-4",
-  };
+    container: `max-w-3xl mx-auto p-6 rounded-lg shadow-md ${darkMode ? "bg-gray-800 text-gray-100" : "bg-white text-gray-900"}`,
+    text: `text-base mb-4 leading-relaxed ${darkMode ? "text-gray-300" : "text-gray-700"}`,
+    heading: `text-2xl font-bold mb-6 ${darkMode ? "text-gray-100" : "text-gray-900"}`,
+    link: `inline-flex items-center ${darkMode ? "text-blue-400 hover:text-blue-300" : "text-blue-600 hover:text-blue-800"} font-medium transition-colors duration-300`,
+  }
 
-  const Biography = () => (
-    <section className="mt-8 max-w-3xl mx-auto">
-      <article className="space-y-6">
+  const ExternalLink: React.FC<{ href: string; children: React.ReactNode }> = ({ href, children }) => (
+    <a href={href} target="_blank" rel="noopener noreferrer" className={styles.link}>
+      {children}
+    </a>
+  )
+
+  return (
+    <div className={`px-4 py-8 ${darkMode ? "bg-gray-900" : "bg-gray-100"} min-h-screen`}>
+      <div className={styles.container}>
+        <h1 className={styles.heading}>About Me</h1>
+        
         <p className={styles.text}>
-          Hi, I'm Alim Idrissou. My passion for mathematics led me to study Planning Science and Mathematics Fundamentals concurrently at university for my Bachelor's degrees. This dual focus taught me valuable lessons in time management and teamwork.
-        </p>
-
-
-        <p className={styles.text}>
-          I had the opportunity to teach mathematics, which I found incredibly rewarding. After completing my degrees, I received a Master's scholarship in Digital Finance in Senegal, where I explored the practical applications of technology in finance.
+          Hello, I'm Alim Idrissou. My journey in technology and finance has been shaped by a love for mathematics and a desire to learn continuously.
         </p>
 
         <p className={styles.text}>
-          My career took a significant turn when I joined the <a href="https://01-edu.org/" target="_blank" rel="noopener noreferrer" className={styles.link}>01 Edu System</a>, where I developed proficiency in Golang, Python, Rust, and JavaScript.
-        </p>
-        <p className={styles.text}>
-          I've had the chance to work on projects with teams from the US, France, and Australia as remote engineers . While I'm still learning, this experience has introduced me to different aspects of AI, from basic machine learning concepts to how AI integrates with other systems.
-        </p>
-        <p className={styles.text}>
-          These projects have been challenging but rewarding. They've pushed me to improve my technical skills and taught me how to communicate better with people from different backgrounds. I'm grateful for these opportunities and am excited to continue learning and growing in this field.
-        </p>
+          
+        I studied Planning Science at <ExternalLink href="https://eneam.uac.bj/">ENEAM</ExternalLink> and Mathematics Fundamentals at <ExternalLink href="https://uac.bj">UAC</ExternalLink> for my Bachelor's degrees, which taught me valuable lessons in time management and teamwork.  Later, I received a Master's scholarship in Digital Finance at <ExternalLink href="https://www.esmt.sn/">ESMT</ExternalLink> in Senegal, where I explored the intersection of technology and finance.
+                  </p>
 
+        <p className={styles.text}>
+        I had the privilege of teaching mathematics, an experience I found deeply rewarding. My professional path led me to the <ExternalLink href="https://01-edu.org/">01 Edu</ExternalLink>, where I've been developing skills in Golang, Python, Rust, and JavaScript. I've had the opportunity to work on projects with teams from various countries as remote engineer, introducing me to different aspects of AI and improving my communication skills across cultures.
+        </p>
 
         <p className={styles.text}>
           Since 2022, I've been volunteering with two projects:
         </p>
-
         <ul className="list-disc pl-6 space-y-2 mb-4">
           <li className={styles.text}>
-            <a href="https://e-smartraining.org/en/" target="_blank" rel="noopener noreferrer" className={styles.link}>E-smart training</a>: Applying technology to solve real-world problems and facilitate learning.
+            <ExternalLink href="https://e-smartraining.org/en/">E-smart training</ExternalLink>: Applying technology to real-world problems and learning.
           </li>
           <li className={styles.text}>
-            <a href="https://rjbdbenindiaspo.com/" target="_blank" rel="noopener noreferrer" className={styles.link}>RJBD</a>: Supporting initiatives for the Beninese diaspora community.
+            <ExternalLink href="https://rjbdbenindiaspo.com/">RJBD</ExternalLink>: Supporting the Beninese diaspora community.
           </li>
         </ul>
 
-
         <p className={styles.text}>
-          I enjoy running, playing football and basketball, and playing strategic video games like PES and FIFA. I am committed to continuous learning and eager to contribute to innovative projects.
+          Outside of work, I enjoy running, playing football and basketball, and strategic video games. I'm a native speaker of <ExternalLink href="https://foodoabee.com/en/linguistics/an-overview-of-foodo-2009">Foodo</ExternalLink> and French, and I'm also fluent in <ExternalLink href="https://en.wikipedia.org/wiki/Dendi_language">Dendi</ExternalLink> and <ExternalLink href="https://en.wikipedia.org/wiki/Fon_language">Fon</ExternalLink>.
         </p>
 
-        <p className={styles.text}>
-          While English isn't my first language, I'm a native speaker of <a href="https://foodoabee.com/en/linguistics/an-overview-of-foodo-2009" target="_blank" rel="noopener noreferrer" className={styles.link}>Foodo</a> and French. I'm also fluent in African languages such as <a href="https://en.wikipedia.org/wiki/Dendi_language" target="_blank" rel="noopener noreferrer" className={styles.link}>Dendi</a> and <a href="https://en.wikipedia.org/wiki/Fon_language" target="_blank" rel="noopener noreferrer" className={styles.link}>Fon</a>, with some knowledge of <a href="https://en.wikipedia.org/wiki/Wolof_language" target="_blank" rel="noopener noreferrer" className={styles.link}>Wolof</a>.
-        </p>
-      </article>
-    </section>
-  );
-
-
-  const PersonalInterests = () => (
-    <section className="mt-12">
-      <p className={styles.text}>
-        I also enjoy engaging in activities that help me relax and rejuvenate, such as praying or reading. Here is a moment of reflection from 2022:
-      </p>
-      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 mt-6">
-        <ImageWithCaption src="/images/alim_praying.png" alt="Alim praying" caption="A moment of reflection" />
-      </div>
-    </section>
-  );
-
-  const ImageWithCaption = ({ src, alt, caption }: { src: string; alt: string; caption: string }) => (
-    <figure className={styles.imageContainer}>
-      <Image
-        src={src}
-        alt={alt}
-        width={300}
-        height={200}
-        className="rounded-lg shadow-md"
-        priority
-      />
-      <figcaption className="text-center mt-2 text-sm text-gray-500">{caption}</figcaption>
-    </figure>
-  );
-
-  return (
-    <>
-      <Head>
-        <title>Alim Idrissou - Biography</title>
-        <meta
-          name="description"
-          content="Learn about Alim Idrissou's journey, expertise, and passions beyond coding."
-        />
-      </Head>
-      <div className={`px-6 py-8 ${darkMode ? "bg-gray-900" : "bg-gray-100"} min-h-screen`}>
-        <div className={styles.container}>
-          <Biography />
-          <PersonalInterests />
-          <footer className="mt-12 pt-4 border-t border-gray-300">
-            <p className={`${styles.text} text-sm italic`}>
-              Note: My legal name is Alimoudine Idrissou.
-            </p>
-          </footer>
+        <div className="mt-8">
+          <p className={styles.text}>
+            Here's a moment from 2022 that captures a personal practice important to me:
+          </p>
+          <figure className="flex flex-col items-center mt-4">
+            <Image
+              src="/images/alim_praying.png"
+              alt="A moment of reflection"
+              width={300}
+              height={200}
+              className="rounded-lg shadow-md"
+            />
+            <figcaption className="text-center mt-2 text-sm text-gray-500">A moment of reflection</figcaption>
+          </figure>
         </div>
-      </div>
-    </>
-  );
-};
 
-export default AboutPage;
+        <footer className="mt-8 pt-4 border-t border-gray-300">
+          <p className={`${styles.text} text-sm italic`}>
+            Note: My legal name is Alimoudine Idrissou.
+          </p>
+        </footer>
+      </div>
+    </div>
+  )
+}
+
+export default AboutPage
