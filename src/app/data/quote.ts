aa2304f8bc7b202571085226fb750eb76da7cf14
ialimoud <i.alim0229@gmail.com>
2024-08-13 23:30:16 +0000

    updates_

--- src/app/data/quote.ts
+++ src/app/data/quote.ts
@@ -7,7 +7,14 @@ const quotes = [
       quote: "It's not true that a straight line is always the shortest route.",
       author: "Gotthold Ephraim Lessing",
     },
+    {
+      quote: "The world is a dangerous place to live, not because of the people who are evil, but because of the people who don't do anything about it.",
+      author: "Albert Einstein",
+    },
 
+
+
+    
  ];
   
   export default quotes;
