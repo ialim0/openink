fae1f96b1be9fb041e0774abea5d43f150f044b2
ialimoud <i.alim0229@gmail.com>
2024-08-15 00:57:14 +0000

    updates_

--- src/app/siteData.tsx
+++ src/app/siteData.tsx
@@ -1,17 +1,17 @@
 const siteData = {
-    title: 'Starter Notion blog for Nextjs with Tailwind',
-    author: 'Notion Blog Starter',
-    headerTitle: 'Nextjs Notion Blog Starter',
-    profileUrl: '/nextjs-logo.png',
-    headerDescription: 'Starter Notion blog for Nextjs with Tailwind',
-    footerText: '© All rights reserved',
-    language: 'en-us',
-    locale: 'en-US',
-    ogBanner: 'https://nextjs-notion-blog-starter.vercel.app/preview/1.jpg',
-    github: 'https://github.com/tuanphungcz/nextjs-notion-blog-starter',
-    twitter: 'https://github.com/tuanphungcz/nextjs-notion-blog-starter',
-    websiteUrl: 'https://nextjs-notion-blog-starter.vercel.app',
-    linkedin: 'https://github.com/tuanphungcz/nextjs-notion-blog-starter'
-  };
-  
-  export default siteData;
+  title: 'Alim Idrissou - Software Engineer',
+  author: 'Alim Idrissou',
+  headerTitle: 'Alim Idrissou',
+  profileUrl: '/profile.jpg',
+  headerDescription: 'Software Engineer | AI Enthusiast',
+  footerText: '© 2023 Alim Idrissou. All rights reserved.',
+  language: 'en-us',
+  locale: 'en-US',
+  ogBanner: '/og-banner.jpg',
+  github: 'https://github.com/0alim0',
+  twitter: 'https://twitter.com/ialim0',
+  websiteUrl: 'https://alimidrissou.com',
+  linkedin: 'https://www.linkedin.com/in/ialim'
+};
+
+export default siteData;
