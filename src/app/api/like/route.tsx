4dc806c43a9842b8168e03d80c137e47c10807e1
ialim0 <i.alim0229@gmail.com>
2025-05-31 17:21:25 -0700

    cleaning

--- src/app/api/like/route.tsx
+++ src/app/api/like/route.tsx
@@ -16,8 +16,16 @@ export async function POST(req: NextRequest): Promise<NextResponse> {
     return new NextResponse('Slug not found', { status: 400 });
   }
 
-  const ip = req.ip;
-  if (ip) {
+  const ip = req.headers.get('x-forwarded-for') || 
+            req.headers.get('x-real-ip') || 
+            req.headers.get('cf-connecting-ip') || 
+            req.headers.get('x-client-ip') ||
+            req.headers.get('x-forwarded') ||
+            req.headers.get('forwarded-for') ||
+            req.headers.get('forwarded') ||
+            'unknown';
+
+  if (ip && ip !== 'unknown') {
     const buf = await crypto.subtle.digest(
       'SHA-256',
       new TextEncoder().encode(ip),
