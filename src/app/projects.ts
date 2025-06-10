33020651585f831addda159e60f4f7054cfd9991
ialim0 <i.alim0229@gmail.com>
2025-06-10 15:51:44 -0700

    Projects Images Added

--- src/app/projects.ts
+++ src/app/projects.ts
@@ -4,7 +4,7 @@ export const projects = [
     name: 'CVBABA – Career Platform for Emerging Markets',
     description:
       'CVBABA is an innovative, multilingual AI assistant that streamlined ATS-optimized résumé and cover letter generation from natural language, solving the challenge of creating beautiful documents regardless of language or design skills.',
-    imageUrls: ['/images/cvbaba-1.png', '/images/cvbaba-2.png'],
+    imageUrls: ['/images/cvbaba-1.png', '/images/cvbaba-2.png', '/images/cvbaba-3.png', '/images/cvbaba-4.png'],
     tags: ['ML', 'NLP', 'Multilingual AI', 'Creativity', 'Python'],
     links: {
       github: '',
